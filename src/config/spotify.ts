e8b999b2d8de1cbbc7dcae5c8fb8d2b09415f991
Zeus Morley <89137497+ZeusMorley@users.noreply.github.com>
2025-09-11 12:24:17 +0800

    Fix

--- src/config/spotify.ts
+++ src/config/spotify.ts
@@ -5,9 +5,14 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 export const spotifyConfig = {
-  clientId: process.env.CLIENT_ID_SPOTIFY || '',
-  clientSecret: process.env.CLIENT_SECRET_SPOTIFY || ''
-};
+    get clientId() {
+      return process.env.CLIENT_ID_SPOTIFY || "";
+    },
+    get clientSecret() {
+      return process.env.CLIENT_SECRET_SPOTIFY || "";
+    }
+  };
+  
 
 export const AUTH_URL = 'https://accounts.spotify.com/api/token';
 export const PLAYLIST_URL = 'https://api.spotify.com/v1/playlists/';
